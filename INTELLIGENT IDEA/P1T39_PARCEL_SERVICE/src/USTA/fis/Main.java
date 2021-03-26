ea609ccce4329fe3dd459678722e6502ad88ecb5
juan_07 <villanuevaariasjuanfelipe@gmail.com>
2021-03-26 14:02:23 -0400

    java
    .

---
+++ INTELLIGENT IDEA/P1T39_PARCEL_SERVICE/src/USTA/fis/Main.java
@@ -0,0 +1,22 @@
+package USTA.fis;
+
+import java.util.Scanner;
+
+/* AUTHOR: Juan Felipe Villanueva Arias
+Date: 25-March-2021
+DESCRIPTION: In this software you can see the cost of somethings products for differents countrus
+ */
+public class Main {
+Scanner= new java.util.Scanner.Keyboard ();
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static int p_choose_the_location (){
+        int v_choose_number =0;
+        System.out.println("Please choose the ubication for send the parcel: 1 for North America, 2 for Central America, 3 for south America and 4 to Euroope");
+
+        while (v_choose_number <1, v_choose_number>5)
+            System.err.println("Err: please put any number that you like between 1 or 4");
+    Scanner.keyboard...
+    }
+}
